7004232452124c14032bd3dd709bfbe6a674ed86
Erik Hide Sæternes <erikhs@simula.no>
2023-10-05 13:29:20 +0200

    Delete build directory

--- build/doc/doxygen/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/erikhs/MatrixProblems/doc/doxygen
-# Build directory: /home/erikhs/MatrixProblems/build/doc/doxygen
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/dune/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/erikhs/MatrixProblems/dune
-# Build directory: /home/erikhs/MatrixProblems/build/dune
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("MatrixProblems")
