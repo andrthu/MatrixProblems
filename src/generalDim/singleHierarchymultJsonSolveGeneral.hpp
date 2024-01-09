0b1b5b98d30ada88cc8a31ae871b3a831cd57dba
andrthu <andrthu@student.matnat.uio.no>
2024-01-09 19:46:30 +0100

    Add singleHierarchymultJsonSolveGeneral executable

--- src/generalDim/singleHierarchymultJsonSolveGeneral.hpp
+++ src/generalDim/singleHierarchymultJsonSolveGeneral.hpp
@@ -1,5 +1,5 @@
 /*
-  Copyright 2023 Andreas Thune.
+  Copyright 2024 Andreas Thune.
 
   This file is part of the Open Porous Media project (OPM).
 
@@ -17,93 +17,11 @@
   along with OPM.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef OPM_MULTJSONSOLVEGENERAL_HEADER_INCLUDED
-#define OPM_MULTJSONSOLVEGENERAL_HEADER_INCLUDED
+#ifndef OPM_SINGLEHIERARCHYMULTJSONSOLVEGENERAL_HEADER_INCLUDED
+#define OPM_SINGLEHIERARCHYMULTJSONSOLVEGENERAL_HEADER_INCLUDED
 
-#endif // OPM_MULTJSONSOLVEGENERAL_HEADER_INCLUDED
+#endif // OPM_SINGLEHIERARCHYMULTJSONSOLVEGENERAL_HEADER_INCLUDED
 
-template<class Mat, class Vec>
-void gen_dim_jsonSolve_mult_sys(std::vector<std::string> systemDirs)
-{
-    typedef Dune::MPIHelper::MPICommunicator MPICommunicator;
-    typedef Dune::CollectiveCommunication<MPICommunicator> CollectiveCommunication;
-    typedef Dune::BiCGSTABSolver<Vec> Solver;
-    typedef Dune::InverseOperatorResult Stat;
-    
-    typedef Dune::OwnerOverlapCopyCommunication<int,int> Comm;
-    typedef Dune::OverlappingSchwarzScalarProduct<Vec,Comm> ScalarProduct;
-    typedef GhostLastMatrixAdapter<Mat,Vec,Vec,Comm> GLO;                 // solveParallel/ghostLastOperations.hpp
-    typedef Dune::OverlappingSchwarzOperator<Mat,Vec,Vec,Comm> Operator;
-    typedef Opm::ParallelOverlappingILU0<Mat,Vec,Vec,Comm> ILU;
-    typedef Dune::FlexibleSolver<Mat, Vec> FlexibleSolverType;
-
-    const auto block_size = Vec::block_type::dimension;
-    
-    CollectiveCommunication cc(MPI_COMM_WORLD);
-    int rank = cc.rank();
-
-    std::vector<Mat> systems;
-    std::vector<Vec> rhs;
-
-    std::vector<ScalarProduct> sps;
-    
-    DictRead DR;
-    Comm comm(cc);
-    std::shared_ptr<Comm> parComm(new(Comm));
-    std::vector<int> mpiVec;
-    for (int i = 0; i < systemDirs.size(); ++i) {
-	
-	if ( boost::algorithm::ends_with( systemDirs[i], ".json") ) {
-	    DR.dict[12] = systemDirs[i];
-	}
-	else {
-	    Mat A_loc;
-	    Vec rhs_loc;
-	    mpiVec = readMatOnRootAndDist(systemDirs[i], A_loc, rhs_loc, DR, comm, parComm, cc, mpiVec, true, i!=0);
-
-	    systems.push_back(A_loc);
-	    rhs.push_back(rhs_loc);
-	    sps.push_back(ScalarProduct(*parComm));
-	}
-    }
-
-    if (rank == 0) {std::cout << std::endl;}
-
-    Opm::FlowLinearSolverParameters flsp_json;
-    flsp_json.linsolver_ = DR.dict[12];
-
-    Opm::PropertyTree prm_json(flsp_json.linsolver_);
-    std::string pc_Type = prm_json.get<std::string>("preconditioner.type");
-    if (pc_Type == "cpr") {
-	prm_json.put("preconditioner.coarsesolver.preconditioner.verbosity", 10);
-    }
-    prm_json.put("preconditioner.verbosity", 10);
-    prm_json.put("verbosity", 2);
-    
-    int pidx = 1;
-    if (block_size == 2)
-	pidx = 0;
-    
-    for (int i = 0; i < systems.size(); ++i) {
-
-	
-	std::function<Vec()> quasi;
-	auto Ai = systems[i];
-	quasi = [Ai, pidx]() {
-	    return Opm::Amg::getQuasiImpesWeights<Mat, Vec>(Ai, pidx, false);
-	};
-
-	GLO glo(Ai, *parComm);
-	auto fs_json = std::make_unique<FlexibleSolverType>(glo, *parComm, prm_json, quasi, pidx);
-
-	Vec crhs(rhs[i]);
-	Vec x(crhs.size());
-	x=0;
-
-	Dune::InverseOperatorResult stat;
-	fs_json->apply(x, crhs, prm_json.get<double>("tol", 0.001), stat);
-    }
-}
 
 template<class Mat, class Vec>
 void gen_dim_jsonSolve_mult_sys_same_hir(std::vector<std::string> systemDirs)
