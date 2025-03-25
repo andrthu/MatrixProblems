c97b3bae3be8721bff7deac4bd6c0b064d358a68
Erik Hide Sæternes <erikhs@simula.no>
2025-03-25 17:43:24 +0100

    Delete build_x86_64 directory

--- build_x86_64/doc/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/erikhs/MatrixProblems/doc
-# Build directory: /home/erikhs/MatrixProblems/build_x86_64/doc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("doxygen")
--- build_x86_64/m4/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/erikhs/MatrixProblems/m4
-# Build directory: /home/erikhs/MatrixProblems/build_x86_64/m4
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
